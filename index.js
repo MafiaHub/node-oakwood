2c89915c6a82a221d09550be71d94e25bee5bb6a
Vladyslav Hrytsenko <vladgritsenko@gmail.com>
2019-08-09 17:00:10 +0300

    moved and renamed constants

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-const constants = {
-    OAK_VISIBILITY_NAME      : 0,
-    OAK_VISIBILITY_ICON      : 1,
-    OAK_VISIBILITY_RADAR     : 2,
-    OAK_VISIBILITY_MODEL     : 3,
-    OAK_VISIBILITY_COLLISION : 4,
-
-    OAK_SEAT_FRONT_LEFT      : 0,
-    OAK_SEAT_FRONT_RIGHT     : 1,
-    OAK_SEAT_REAR_LEFT       : 2,
-    OAK_SEAT_REAR_RIGHT      : 3,
-}
 
 module.exports = Object.assign({},
     require('./src/main.js'),
     require('./src/players.js'),
     require('./src/vehicles.js'),
-    constants,
+    require('./src/constants.js')
 )
---
+++ src/constants.js
@@ -0,0 +1,12 @@
+module.exports = {
+    VISIBILITY_NAME      : 0,
+    VISIBILITY_ICON      : 1,
+    VISIBILITY_RADAR     : 2,
+    VISIBILITY_MODEL     : 3,
+    VISIBILITY_COLLISION : 4,
+
+    SEAT_FRONT_LEFT      : 0,
+    SEAT_FRONT_RIGHT     : 1,
+    SEAT_REAR_LEFT       : 2,
+    SEAT_REAR_RIGHT      : 3,
+}
